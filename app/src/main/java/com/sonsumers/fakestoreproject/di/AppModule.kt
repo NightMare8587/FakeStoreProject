1dcd6c8657a1c04738b76314dba26ae64a4c59aa
pulkit <pulkit.loya@myn.global>
2024-05-20 10:37:31 +0530

    created basic UI for product composable. need to create viewModel data and send it to composable

--- app/src/main/java/com/sonsumers/fakestoreproject/di/AppModule.kt
+++ app/src/main/java/com/sonsumers/fakestoreproject/di/AppModule.kt
@@ -1,13 +1,35 @@
 package com.sonsumers.fakestoreproject.di
 
+import com.sonsumers.fakestoreproject.network.ApiCalls
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun providesRetrofitApiInstance() : ApiCalls {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        return retrofit.create(ApiCalls::class.java)
+    }
 
 }
